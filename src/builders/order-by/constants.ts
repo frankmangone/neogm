40ac59fb4176e90079bf5a7e6730a39f8c03f607
frankmangone <francomangone18@gmail.com>
2023-09-03 16:01:07 -0300

    Adds tests for the order by builder

--- src/builders/order-by/constants.ts
+++ src/builders/order-by/constants.ts
@@ -1,3 +1,8 @@
 export const ASC = "ASC";
 export const DESC = "DESC";
 export const ORDER_BY = "ORDER BY";
+
+export enum Order {
+	ASC = "ASC",
+	DESC = "DESC",
+}
---
+++ tests/builders/order-by.builder.spec.ts
@@ -0,0 +1,59 @@
+import { OrderByBuilder, ASC, DESC, Order } from "~/builders/order-by";
+
+describe("OrderByBuilder", () => {
+	let builder: OrderByBuilder;
+
+	beforeEach(() => {
+		builder = new OrderByBuilder();
+	});
+
+	describe("Method: constructor", () => {
+		it("should initialize without any cypher statement", () => {
+			expect(builder.cypher).toBeUndefined();
+		});
+	});
+
+	describe("Method: constructor", () => {
+		it("should allow ordering by a property without specifying ASC or DESC", () => {
+			builder.orderBy({ property: "name" }).done();
+			expect(builder.cypher).toBe("ORDER BY name");
+		});
+
+		it("should allow ordering by a property in ascending order", () => {
+			builder.orderBy({ property: "name", order: ASC }).done();
+			expect(builder.cypher).toBe("ORDER BY name ASC");
+		});
+
+		it("should allow ordering by a property in descending order", () => {
+			builder.orderBy({ property: "age", order: DESC }).done();
+			expect(builder.cypher).toBe("ORDER BY age DESC");
+		});
+
+		it("should allow specifying order using the provided enum `Order`", () => {
+			builder.orderBy({ property: "age", order: Order.DESC }).done();
+			expect(builder.cypher).toBe("ORDER BY age DESC");
+		});
+
+		it("should allow ordering by multiple properties", () => {
+			builder
+				.orderBy({ property: "name", order: Order.ASC })
+				.orderBy({ property: "age", order: Order.DESC })
+				.done();
+			expect(builder.cypher).toBe("ORDER BY name ASC, age DESC");
+		});
+
+		it("should throw an error if no properties are specified to order by", () => {
+			expect(() => builder.done()).toThrow(
+				"No properties to order by were specified."
+			);
+		});
+
+		it("should allow multiple calls to orderBy for the same property, taking the last one", () => {
+			builder
+				.orderBy({ property: "name", order: ASC })
+				.orderBy({ property: "name", order: DESC })
+				.done();
+			expect(builder.cypher).toBe("ORDER BY name DESC");
+		});
+	});
+});
